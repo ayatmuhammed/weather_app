1337939da271c169c368d31d34f25c03baf1e633
iraqi <iraqi.078870@gmail.com>
2020-07-29 18:00:07 +0300

    test

--- lib/ui/splashScreen.dart
+++ lib/ui/splashScreen.dart
@@ -3,7 +3,6 @@ class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
-
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
@@ -16,6 +15,5 @@ class _SplashScreenState extends State<SplashScreen> {
       ),
     );
   }
-
 }
 
