81b97da462c03f096c669e188f81a6d4b2ecb2d5
iraqi <iraqi.078870@gmail.com>
2020-08-17 16:39:17 +0300

    flavor

---
+++ lib/adminMain.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:weatherapp/ui/Admin/AddJok.dart';
+void main() => runApp(MaterialApp(
+  home:AddJok(),
+)
+);
+
---
+++ lib/ui/Admin/AddJok.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AddJok extends StatefulWidget {
+  @override
+  _AddJokState createState() => _AddJokState();
+}
+
+class _AddJokState extends State<AddJok> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.blue,);
+  }
+}
