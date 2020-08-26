3b481ba109ebcfcb6a1c791b64ecd3f1ba828122
iraqi <iraqi.078870@gmail.com>
2020-08-26 23:19:15 +0300

    image

--- lib/ui/Home.dart
+++ lib/ui/Home.dart
@@ -4,13 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:weatherapp/ui/searchClass.dart';
 import 'package:translator/translator.dart';
 
 class Homepage extends StatefulWidget {
   String myCity;
-  Homepage({Key key, this.myCity = 'baghdad'}) : super(key: key);
-
+  Homepage({Key key, this.myCity = 'بغداد'}) : super(key: key);
   @override
   _HomepageState createState() => _HomepageState();
 }
@@ -50,23 +50,27 @@ var firestore=Firestore.instance;
                           if (snap.hasData)
                             return Column(
                               children: <Widget>[
-                                StreamBuilder<DocumentSnapshot>(
-                                  stream:firestore.collection('icons').document('${snap.data['weather'][0]['icon']}').snapshots(),
-                                    builder: (context, AsyncSnapshot<DocumentSnapshot> snp){
-                                 if( snp.connectionState==ConnectionState.waiting)return CircularProgressIndicator();
-                                      else if(snp.hasData) return Container(
-                                     height: 100,width: 100,
-                                   child: Image.network(
-                                     snp.data['img'],loadingBuilder: (context, child, loadingProgress) =>
-                                       loadingProgress==null?child:CircularProgressIndicator()
-                                     ,),
+                                StreamBuilder<QuerySnapshot>(
+                                  stream:firestore.collection('icons').where(FieldPath.documentId,isEqualTo: snap.data['weather'][0]['icon']).snapshots(),
+                                    builder: (context, AsyncSnapshot<QuerySnapshot> snp){
+                                    if(snp.hasData){
+                                        List img=snp.data.documents.map((e) => e).toList();
+                                      if(img.length>0)
+                                        return Container(
+                                     height: 200,width: 200,
+                                   child: CachedNetworkImage(
+                                       fit: BoxFit.cover,
+                                       imageUrl: img[0]['img']??' '),
                                  );
-                                      else return  Image.network(
-                                    'https://openweathermap.org/img/wn/${snap.data['weather'][0]['icon']}@2x.png');
+                                      else return CachedNetworkImage(imageUrl: 'https://openweathermap.org/img/wn/${snap.data['weather'][0]['icon']}@2x.png'
+                                        ,fit: BoxFit.cover,);
+                                    }
+                                      else return  CachedNetworkImage(imageUrl: 'https://openweathermap.org/img/wn/${snap.data['weather'][0]['icon']}@2x.png'
+                                      ,fit: BoxFit.cover,);
                                 }),
 //
                                 Text(
-                                  snap.data['name'],
+                                  widget.myCity,
                                   style: TextStyle(
                                       color:Colors.black,
                                       fontSize: 30.0,
@@ -74,21 +78,22 @@ var firestore=Firestore.instance;
                                 ),
                                 FutureBuilder(
                                   future: translate(snap.data['weather'][0]['description']),
-                                    builder: (context,value)=>Text(
-                                      value.toString(),
+                                    builder: (context,value)=>
+                                  value.hasData? Text(
+                                      value.data.toString()??' ',
                                       style: TextStyle(
                                         color: Colors.black,
                                         fontSize: 20.0,
                                       ),
-                                    ),),
-
+                                    ):Container(),),
                                 Text(
                                   ((snap.data['main']['temp']).toInt() -
                                           273)
                                       .toString(),
                                   style: TextStyle(
                                     color: Colors.black,
-                                    fontSize: 80.0,
+                                    fontSize: 100.0,
+                                    fontFamily: 'GESS'
                                   ),
                                 ),
 
@@ -110,7 +115,6 @@ var firestore=Firestore.instance;
                             return Center(child: Text('Wait'));
                         }),
                     Image.asset('images/wind-plant (1).gif',height:150.0,width:150.0,),
-
                   ],
                 ),
           ),
@@ -118,7 +122,6 @@ var firestore=Firestore.instance;
       ),
     );
   }
-
 //  Map details = new Map();
   Future<Map> getData(String city) async {
     var response = await http.get(
@@ -129,9 +132,8 @@ var firestore=Firestore.instance;
     return data;
   }
 
-  Future<String>translate(String word)async{
+  Future translate(String word)async{
     final translator = GoogleTranslator();
-  return await translator.translate(word, from: 'en', to: 'ar').then((value) => value.toString());
-
+  return await translator.translate(word, from: 'en', to: 'ar').then((value) => value);
   }
 }
