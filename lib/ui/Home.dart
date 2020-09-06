77f05c026370d0e64228fa7adb57d5594cbfb7d1
break a leg <nonahuda26@gmail.com>
2020-09-06 17:13:33 +0300

    هايه الاخطاء امير

--- lib/ui/Home.dart
+++ lib/ui/Home.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:core';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -13,46 +14,77 @@ import 'package:wave/wave.dart';
 import 'package:weatherapp/ui/searchClass.dart';
 import 'package:translator/translator.dart';
 import 'package:intl/intl.dart' as Intl;
-//
-//
-// const String testDevice="";
+import 'package:firebase_admob/firebase_admob.dart';
+
+const String testDevice = "";
 
 class Homepage extends StatefulWidget {
-  // static final MobileAdTargetingInfo targetingInfo=new MobileAdTargetingInfo(
-  //   testDevices: testDevice !=null?<String>[testDevice]:null,
-  //   keywords: <String>['weather','jokes',"weather predication's"],
-  //   birthday: DateTime.now(),
-  //   childDirected: true,
-  // );
   String myCity;
-  Homepage({Key key, this.myCity="بغداد"}) : super(key: key);
+  Homepage({Key key, this.myCity = "بغداد"}) : super(key: key);
   @override
   _HomepageState createState() => _HomepageState();
 }
 
 class _HomepageState extends State<Homepage> {
+  static final MobileAdTargetingInfo targetingInfo = new MobileAdTargetingInfo(
+    testDevices: <String>[],
+    keywords: <String>['weather', 'jokes', "weather predication's"],
+    birthday: DateTime.now(),
+    childDirected: true,
+  );
+  BannerAd _bannerAd;
+  InterstitialAd _interstitialAd;
+  BannerAd showBannerAd() {
+    return BannerAd(
+        adUnitId: "ca-app-pub-2609542594798987/3620052400",
+        size: AdSize.largeBanner,
+        targetingInfo: targetingInfo,
+        listener: (MobileAdEvent event) {
+          print("Banner Event :$event");
+        });
+  }
+
+  InterstitialAd showInterstitialAd() {
+    return InterstitialAd(
+        adUnitId: "ca-app-pub-2609542594798987/4549990698",
+        targetingInfo: targetingInfo,
+        listener: (MobileAdEvent event) {
+          print("InterstitialAd Event :$event");
+        });
+  }
+
   var firestore = Firestore.instance;
   VideoPlayerController _controller;
-  Color color =Colors.black;
-  int date =int.parse(Intl.DateFormat('kk').format( DateTime.now()));
+  Color color = Colors.black;
+  int date = int.parse(Intl.DateFormat('kk').format(DateTime.now()));
   @override
   void initState() {
-    color=(date>=6 && date<18)?Colors.black:Colors.white;
+    color = (date >= 6 && date < 18) ? Colors.black : Colors.white;
     firestore.settings(persistenceEnabled: true);
     getCity().then((value) {
       setState(() {
-        if(value!=null)
-        widget.myCity=value;
+        if (value != null) widget.myCity = value;
       });
     });
     super.initState();
-    _controller = VideoPlayerController.asset(
-        'assets/Moon.mp4')
+    FirebaseAdMob.instance.initialize(appId: "ca-app-pub-2609542594798987~5330743414");
+    _bannerAd = showBannerAd()
+      ..load()
+      ..show();
+
+    _controller = VideoPlayerController.asset('assets/Moon.mp4')
       ..initialize().then((_) {
         _controller.play();
         setState(() {});
       });
     _controller.setLooping(true);
+  }
+
+  @override
+  void dispose() {
+    _bannerAd?.dispose();
+    _interstitialAd?.dispose();
+    super.dispose();
   }
 
   @override
@@ -64,95 +96,106 @@ class _HomepageState extends State<Homepage> {
             if (snap.hasData)
               return Stack(
                 children: [
-                  (date >=6 && date<18) ?  Positioned(
-                    top: 10.0,
-                    child: StreamBuilder<DocumentSnapshot>(
-              stream: firestore
-                  .collection('pictures')
-                  .document('img')
-                  .snapshots(),
-            builder: (context, AsyncSnapshot<DocumentSnapshot> snp) {
-              if (snp.hasData) {
-                return Container(
-                  height: MediaQuery.of(context).size.height/2,
-                  width: MediaQuery.of(context).size.width,
-                  child: CachedNetworkImage(
-                      fit: BoxFit.cover,
-                      imageUrl: snp.data[snap.data['name']] ?? ' '),
-                );
-              } else
-                return Container(
-                  width: 400.0,
-                  height: 400.0,
-                  child: Image.asset('images/wind-plant (1).gif'),
-                );
-            }
-            ),
-                  ):
-                      Positioned(
-                       top: 10,
+                  (date >= 6 && date < 18)
+                      ? Positioned(
+                          top: 10.0,
+                          child: StreamBuilder<DocumentSnapshot>(
+                              stream: firestore
+                                  .collection('pictures')
+                                  .document('img')
+                                  .snapshots(),
+                              builder: (context,
+                                  AsyncSnapshot<DocumentSnapshot> snp) {
+                                if (snp.hasData) {
+                                  return Container(
+                                    height: MediaQuery.of(context).size.height /
+                                        1.5,
+                                    width: MediaQuery.of(context).size.width,
+                                    child: CachedNetworkImage(
+                                        fit: BoxFit.cover,
+                                        imageUrl:
+                                            snp.data[snap.data['name']] ?? ' '),
+                                  );
+                                } else
+                                  return Container(
+                                    width: 400.0,
+                                    height: 400.0,
+                                    child: Image.asset(
+                                        'images/wind-plant (1).gif'),
+                                  );
+                              }),
+                        )
+                      : Positioned(
+                          top: 10,
                           child: Container(
                             width: MediaQuery.of(context).size.width,
-                              height:MediaQuery.of(context).size.height,
-                              child: _controller.value.initialized?AspectRatio(
-                                aspectRatio:_controller.value.aspectRatio ,
-                                  child: VideoPlayer(_controller)):Center(child: CircularProgressIndicator(),),
-                          )
-                      ),
-                  (date >=6 && date<18) ?  Positioned(
-                    top: 350,
-                    child: WaveWidget(
-                      config: CustomConfig(
-                        gradients: [
-                          [
-                            Color.fromRGBO(72, 74, 126, 1),
-                            Color.fromRGBO(125, 170, 206, 1),
-                            Color.fromRGBO(184, 189, 245, 0.7),
-                          ],
-                          [
-                            Color.fromRGBO(72, 74, 126, 1),
-                            Color.fromRGBO(125, 170, 206, 1),
-                            Color.fromRGBO(172, 182, 219, 0.7)
-                          ],
-                          [
-                            Color.fromRGBO(255, 255, 255,1),
-                            Color.fromRGBO(255, 255, 255, 1),
-                            Color.fromRGBO(255, 255, 255, 1)
-                          ]
-
-                        ],
-                        durations: [6000, 10800, 10000],
-                        heightPercentages: [0.03, 0.01, 0.04],
-                         gradientBegin: Alignment.bottomCenter,
-                         gradientEnd: Alignment.bottomCenter,
-                      ),
-                 size: Size(
-                MediaQuery.of(context).size.width,
-                MediaQuery.of(context).size.height /2,
-              ),
-                     // backgroundColor: Colors.white,
-                    ),
-                  ):SizedBox(),
-
-                  Center(
+                            height: MediaQuery.of(context).size.height,
+                            child: _controller.value.initialized
+                                ? AspectRatio(
+                                    aspectRatio: _controller.value.aspectRatio,
+                                    child: VideoPlayer(_controller))
+                                : Center(
+                                    child: CircularProgressIndicator(),
+                                  ),
+                          )),
+                  (date >= 6 && date < 18)
+                      ? Positioned(
+                          top: 250,
+                          child: WaveWidget(
+                            config: CustomConfig(
+                              gradients: [
+                                [
+                                  Color.fromRGBO(72, 74, 126, 1),
+                                  Color.fromRGBO(125, 170, 206, 1),
+                                  Color.fromRGBO(184, 189, 245, 0.7),
+                                ],
+                                [
+                                  Color.fromRGBO(72, 74, 126, 1),
+                                  Color.fromRGBO(125, 170, 206, 1),
+                                  Color.fromRGBO(172, 182, 219, 0.7),
+                                ],
+                                [
+                                  Color.fromRGBO(255, 255, 255, 1),
+                                  Color.fromRGBO(255, 255, 255, 1),
+                                  Color.fromRGBO(255, 255, 255, 1),
+                                ]
+                              ],
+                              durations: [6000, 10800, 10000],
+                              heightPercentages: [0.03, 0.01, 0.04],
+                              gradientBegin: Alignment.bottomCenter,
+                              gradientEnd: Alignment.bottomCenter,
+                            ),
+                            size: Size(
+                              MediaQuery.of(context).size.width,
+                              MediaQuery.of(context).size.height,
+                            ),
+                            // backgroundColor: Colors.white,
+                          ),
+                        )
+                      : SizedBox(),
+                  Positioned(
+                    top: 340,
+                    left: 100,
                     child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.center,
+                      // mainAxisAlignment: MainAxisAlignment.end,
+                      // crossAxisAlignment: CrossAxisAlignment.center,
                       children: <Widget>[
                         Text(
                           widget.myCity,
                           style: TextStyle(
-                              color: color,
-                              fontSize: 30.0,
-                              fontWeight: FontWeight.bold),
+                            color: color,
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                         FutureBuilder(
-                          future: translate(snap.data['weather'][0]['description']),
+                          future:
+                              translate(snap.data['weather'][0]['description']),
                           builder: (context, value) => value.hasData
                               ? Text(
                                   value.data.toString() ?? ' ',
                                   style: TextStyle(
-                                    color:color,
+                                    color: color,
                                     fontSize: 20.0,
                                   ),
                                 )
@@ -162,9 +205,7 @@ class _HomepageState extends State<Homepage> {
                           ((snap.data['main']['temp']).toInt() - 273)
                               .toString(),
                           style: TextStyle(
-                              color: color,
-                              fontSize: 50.0,
-                              fontFamily: 'GESS'),
+                              color: color, fontSize: 50.0, fontFamily: 'GESS'),
                         ),
                         StreamBuilder<DocumentSnapshot>(
                             stream: firestore
@@ -197,7 +238,6 @@ class _HomepageState extends State<Homepage> {
                       ],
                     ),
                   ),
-
                   Positioned(
                     right: 50,
                     top: 50,
@@ -242,14 +282,16 @@ class _HomepageState extends State<Homepage> {
                         }),
                   ),
                   Padding(
-                    padding: const EdgeInsets.only(top:70,left: 20),
+                    padding: const EdgeInsets.only(top: 60, left: 20),
                     child: IconButton(
                       icon: Icon(
                         Icons.search,
-                        color:color,
-                        size: 40,
+                        color: color,
+                        size: 30,
                       ),
+
                       onPressed: () {
+                        showInterstitialAd()..load()..show();
                         showSearch(
                             context: context,
                             delegate: DataSearch(con: context));
@@ -283,8 +325,9 @@ class _HomepageState extends State<Homepage> {
         .translate(word, from: 'en', to: 'ar')
         .then((value) => value);
   }
- Future<String> getCity() async {
+
+  Future<String> getCity() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-   return await prefs.getString('city');
+    return await prefs.getString('city');
   }
 }
